80d9dbd3f92be0eed8b03a1c8022af3320cfc700
John Kim <jksfo@pm.me>
2020-07-27 18:53:31 -0700

    fix dev db connection

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+};
 
-const prodConfig = process.env.DATABASE_URL;
+// const devConfig = {
+//   connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+// }
 
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? prodConfig : devConfig,
-});
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL
+};
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? prodConfig : devConfig);
 
 module.exports = pool;
